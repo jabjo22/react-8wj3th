2ac62bbc6df89e0518911aaa9bb440e8cb7f25a8
jabjo22 <jarlebjo@hotmail.com>
2020-11-05 19:12:07 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import axios from 'axios'
 import "./style.css";
 
 export default function App() {
+  
+ const getData = () => {​​​​​ axios.get(`https://ReplitExpress.magneka.repl.co`) .then(result => {​​​​​ console.log(result) }​​​​​) .catch(error => {​​​​​ console.log(error) }​​​​​) }​​​​​ ​
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+      <button onClick={getData}>Klikk på meg</button>
+
     </div>
   );
 }
